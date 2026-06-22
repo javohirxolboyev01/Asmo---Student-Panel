807d3141247d4ec3e82db0c762add9052d77ad71
Javohir <javohirxolboyev0232@gmail.com>
2026-06-22 16:58:50 +0500

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layouts/Layout";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import PublicRoute from "./routes/PublicRoute";
 
 // Lazy load pages
 const Home = lazy(() => import("./pages/Home"));
